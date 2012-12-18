511e1876b0f6bd065cafb811515b41152eed46a1
Julian Heise <ms@SWE-Admins-MacBook-Pro.local>
2012-12-18 12:48:38 +0100

    view dummy

---
+++ ios-timeline/BHTTimeLineView.h
@@ -0,0 +1,13 @@
+//
+//  BHTTimeLineView.h
+//  ios-timeline
+//
+//  Created by Master-Student on 18.12.12.
+//  Copyright (c) 2012 Beuth-Hochschule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BHTTimeLineView : UIView
+
+@end
---
+++ ios-timeline/BHTTimeLineView.m
@@ -0,0 +1,45 @@
+//
+//  BHTTimeLineView.m
+//  ios-timeline
+//
+//  Created by Master-Student on 18.12.12.
+//  Copyright (c) 2012 Beuth-Hochschule. All rights reserved.
+//
+
+#import "BHTTimeLineView.h"
+
+@implementation BHTTimeLineView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetLineWidth(context, 2.0);
+    
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    
+    CGFloat components[] = {0.0, 0.0, 1.0, 1.0};
+    
+    CGColorRef color = CGColorCreate(colorspace, components);
+    
+    CGContextSetStrokeColorWithColor(context, color);
+    
+    CGContextMoveToPoint(context, 0, 0);
+    CGContextAddLineToPoint(context, 300, 400);
+    
+    CGContextStrokePath(context);
+    CGColorSpaceRelease(colorspace);
+    CGColorRelease(color);
+}
+
+@end
